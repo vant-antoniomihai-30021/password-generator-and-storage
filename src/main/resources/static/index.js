3aaaa5a688c2bcd7951911b72301a65a7fee6187
Vanț Antonio Mihai <antonio.vant@yahoo.com>
2022-08-22 16:09:12 +0300

    Improvements

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -2,10 +2,26 @@
 var clearBtn = document.getElementById("clear");
 clearBtn.onclick = function clear() {
     document.getElementById("response").innerHTML = "";
+    var clearTag = document.getElementById("clearTag");
+    clearTag.style.color="rgb(196, 255, 240)";
+    clearTag.style.fontSize="22px";
+    clearTag.style.transition="all 1s"
+    setTimeout( function(){ 
+        clearTag.style.fontSize="1em";
+        clearTag.style.color="white";
+    },1000);
 }
 
 
 function httpGet(url) {
+    var getAllTag = document.getElementById("getAllTag");
+    getAllTag.style.color="rgb(196, 255, 240)";
+    getAllTag.style.fontSize="22px";
+    getAllTag.style.transition="all 1s"
+    setTimeout( function(){ 
+        getAllTag.style.fontSize="1em";
+        getAllTag.style.color="white";
+    },1000);
     document.getElementById("response").innerHTML = "";
     fetch(url).then((response) => response.json()).then((date) => { console.log(date);
         for(var i =0;i<date.length;i++){
@@ -31,13 +47,13 @@ function httpGet(url) {
 
 function httpPut(url) {
     var pTag = document.getElementById("getTag");
-    pTag.style.color="aqua";
+    pTag.style.color="rgb(196, 255, 240)";
     pTag.style.fontSize="22px";
-    pTag.style.transition="all 1.5s"
+    pTag.style.transition="all 1s"
     setTimeout( function(){ 
         pTag.style.fontSize="1em";
         pTag.style.color="white";
-    },2000);
+    },1000);
     var xml = new XMLHttpRequest();
     xml.open("Put", url, false);
     xml.send(null);
@@ -46,6 +62,14 @@ function httpPut(url) {
 }
 
 function httpDelete(url) {
+    var deleteTag = document.getElementById("deleteTag");
+    deleteTag.style.color="rgb(196, 255, 240)";
+    deleteTag.style.fontSize="22px";
+    deleteTag.style.transition="all 1s"
+    setTimeout( function(){ 
+        deleteTag.style.fontSize="1em";
+        deleteTag.style.color="white";
+    },1000);
     var id = document.getElementById("passwordId");
     var xml = new XMLHttpRequest();
     xml.open("Delete", url + id.value, false);
@@ -54,6 +78,14 @@ function httpDelete(url) {
 }
 
 function httpDeleteAll(url) {
+    var deleteAllTag = document.getElementById("deleteAllTag");
+    deleteAllTag.style.color="rgb(196, 255, 240)";
+    deleteAllTag.style.fontSize="22px";
+    deleteAllTag.style.transition="all 1s"
+    setTimeout( function(){ 
+        deleteAllTag.style.fontSize="1em";
+        deleteAllTag.style.color="white";
+    },1000);
     var xml = new XMLHttpRequest();
     xml.open("Delete", url, false);
     xml.send(null);
