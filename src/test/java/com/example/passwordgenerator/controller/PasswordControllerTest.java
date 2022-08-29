dc8492066329273b69af3d3c09e7dd03864ec1ce
Vanț Antonio Mihai <antonio.vant@yahoo.com>
2022-08-29 11:31:25 +0300

    working on controller tests

--- src/test/java/com/example/passwordgenerator/controller/PasswordControllerTest.java
+++ src/test/java/com/example/passwordgenerator/controller/PasswordControllerTest.java
@@ -1,10 +1,58 @@
 package com.example.passwordgenerator.controller;
 
+import com.example.passwordgenerator.password.Password;
+import com.example.passwordgenerator.repository.PasswordRepository;
+import com.example.passwordgenerator.service.PasswordService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+
+
+@ExtendWith(MockitoExtension.class)
+@WebMvcTest(PasswordController.class)
 class PasswordControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private PasswordService passwordServiceTest;
+    @MockBean
+    private PasswordRepository passwordRepositoryTest;
 
-    public void shouldGetTheListOfThePasswords() throws Exception {
-
-
+    @Test
+    public void shouldNotBeNullMockMvc(){
+        assertNotNull(mockMvc);
     }
+
+    @Test
+    public void shouldReturnAListWithAllThePasswords() throws Exception{
+        List<Password> list = new ArrayList<>();
+        list.add(new Password());
+        list.add(new Password());
+        Mockito.when(passwordServiceTest.getAllPasswords()).thenReturn(list);
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/passwords")).andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldAddANewPassword()throws Exception{
+        Password passwordToAdd = new Password();
+        Mockito.doNothing().when(passwordServiceTest).saveThisPassword(passwordToAdd);
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/passwords/put")).andExpect(status().isOk());
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/passwords")).andExpect(status().isOk()).andReturn();
+    }
+
+
 
 }
--- src/test/java/com/example/passwordgenerator/service/PasswordServiceTest.java
+++ src/test/java/com/example/passwordgenerator/service/PasswordServiceTest.java
@@ -13,7 +13,6 @@ import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class PasswordServiceTest {
-
     private PasswordService passwordServiceTest;
     @Mock
     private PasswordRepository passwordRepositoryTest;
