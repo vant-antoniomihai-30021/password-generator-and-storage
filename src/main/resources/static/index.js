3824c9694ab588f1b6eda900f8b3d7f57d2565a2
Vanț Antonio Mihai <antonio.vant@yahoo.com>
2022-09-06 11:20:36 +0300

    made the web page be more responsive

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,16 +1,34 @@
 var clearBtn = document.getElementById("clear");
+var mediaScreen = window.matchMedia("(max-width:890px)");
+var mScreen = window.matchMedia("(max-width:801px)");
+var m700Screen = window.matchMedia("(max-width:700px)");
+var m616Screen = window.matchMedia("(max-width:616px)");
+var m852Screen = window.matchMedia("(max-width:852px)");
+
+var change = window.setInterval(function () {
+    if (m852Screen.matches) {
+        document.getElementById("pwToAdd").innerHTML = "(Has to be > 8)";
+    }
+    else document.getElementById("pwToAdd").innerHTML = "(Has to be greater than 8)";
+}, 500);
+
+
 clearBtn.onclick = function clear() {
     document.getElementById("response").innerHTML = "";
     var clearTag = document.getElementById("clearTag");
     clearTag.style.color = "rgb(196, 255, 240)";
-    clearTag.style.fontSize = "22px";
-    clearTag.style.transition = "all 1s"
+    if (mediaScreen.matches != true) {
+        clearTag.style.transition = "all 1s";
+        clearTag.style.fontSize = "20px";
+    }
+    else
+        clearTag.style.transition = "color 1s";
     setTimeout(function () {
         clearTag.style.fontSize = "1em";
         clearTag.style.color = "white";
     }, 1000);
 }
-
+var copy;
 window.onload = function () {
     fetch("http://localhost:8080/api/v1/passwords" + "/check-if-empty").then((data) => data.json()).then((data) => {
         if (data === true) {
@@ -19,7 +37,12 @@ window.onload = function () {
             document.getElementById("response").appendChild(tag);
         }
         else
-            httpGet("http://localhost:8080/api/v1/passwords", 0);
+            {httpGet("http://localhost:8080/api/v1/passwords", 0);
+            fetch("http://localhost:8080/api/v1/passwords").then((data) => data.json()).then((data) => {
+               copy = data;
+            })
+
+    }
     })
 };
 
@@ -29,8 +52,12 @@ function httpGet(url, cal) {
     if (cal == 1) {
         var getAllTag = document.getElementById("getAllTag");
         getAllTag.style.color = "rgb(196, 255, 240)";
-        getAllTag.style.fontSize = "22px";
-        getAllTag.style.transition = "all 1s"
+        if (mediaScreen.matches != true) {
+            getAllTag.style.transition = "all 1s";
+            getAllTag.style.fontSize = "20px";
+        }
+        else
+            getAllTag.style.transition = "color 1s";
         setTimeout(function () {
             getAllTag.style.fontSize = "1em";
             getAllTag.style.color = "white";
@@ -66,6 +93,16 @@ function httpGet(url, cal) {
             tag.classList.add("tagPw");
             var ind = document.createElement("p"); ind.style.display = "inline"; tag.style.display = "inline";
             ind.innerHTML = "Password with the number " + (date[i].indOfThisPassword) + " is: ";
+            ind.setAttribute("id","some-ind");
+            if (mScreen.matches)
+                ind.innerHTML = "Pw with the number " + (date[i].indOfThisPassword) + " is: ";
+
+            if (m700Screen.matches == true)
+                ind.innerHTML = "Pw number " + (date[i].indOfThisPassword) + " is: ";
+
+            if (m616Screen.matches == true)
+                ind.innerHTML = "Pw " + (date[i].indOfThisPassword) + " is: ";
+           
             tag.style.color = "rgb(152, 256, 255)";
             div.appendChild(ind);
             div.appendChild(tag);
@@ -103,11 +140,31 @@ function httpGet(url, cal) {
     );
 }
 
+    // TO WORK ON THIS TOMORROW
+    // window.setInterval(function(){
+    //     var someInd = document.getElementById("some-ind");
+        
+    //     for (var i = 0; i < copy.length; i++) {
+    //     if (mScreen.matches)
+    //     someInd.innerHTML = "Pw with the number " + (copy[i].indOfThisPassword) + " is: ";
+    
+    // if (m700Screen.matches == true)
+    // someInd.innerHTML = "Pw number " + (copy[i].indOfThisPassword) + " is: ";
+    
+    // if (m616Screen.matches == true)
+    // someInd.innerHTML = "Pw " + (copy[i].indOfThisPassword) + " is: ";}
+    // },500)
+
+
 function httpPut(url) {
     var pTag = document.getElementById("getTag");
     pTag.style.color = "rgb(196, 255, 240)";
-    pTag.style.fontSize = "22px";
-    pTag.style.transition = "all 1s"
+    if (mediaScreen.matches != true) {
+        pTag.style.transition = "all 1s";
+        pTag.style.fontSize = "20px";
+    }
+    else
+        pTag.style.transition = "color 1s";
     setTimeout(function () {
         pTag.style.fontSize = "1em";
         pTag.style.color = "white";
@@ -122,21 +179,29 @@ function httpPut(url) {
 function httpDelete(url) {
     var id = document.getElementById("passwordId");
     var deleteTag = document.getElementById("deleteTag");
-    if(id.value==" " || id.value==""){
+    if (id.value == " " || id.value == "") {
         deleteTag.style.color = "red";
-        deleteTag.style.fontSize = "22px";
-        deleteTag.style.transition = "all 1s"
+        if (mediaScreen.matches != true) {
+            deleteTag.style.transition = "all 1s";
+            deleteTag.style.fontSize = "20px";
+        }
+        else
+            deleteTag.style.transition = "color 1s";
         setTimeout(function () {
             deleteTag.style.fontSize = "1em";
             deleteTag.style.color = "white";
         }, 1000);
     }
-    else{
+    else {
         fetch("http://localhost:8080/api/v1/passwords/exists" + id.value).then((data) => data.json()).then((data) => {
             if (data === true) {
                 deleteTag.style.color = "rgb(196, 255, 240)";
-                deleteTag.style.fontSize = "22px";
-                deleteTag.style.transition = "all 1s"
+                if (mediaScreen.matches != true) {
+                    deleteTag.style.transition = "all 1s";
+                    deleteTag.style.fontSize = "20px";
+                }
+                else
+                    deleteTag.style.transition = "color 1s";
                 setTimeout(function () {
                     deleteTag.style.fontSize = "1em";
                     deleteTag.style.color = "white";
@@ -146,24 +211,33 @@ function httpDelete(url) {
                 xml.send(null);
                 httpGet('http://localhost:8080/api/v1/passwords', 0);
             }
-            else{
+            else {
                 deleteTag.style.color = "red";
-                deleteTag.style.fontSize = "22px";
-                deleteTag.style.transition = "all 1s"
+                if (mediaScreen.matches != true) {
+                    deleteTag.style.transition = "all 1s";
+                    deleteTag.style.fontSize = "20px";
+                }
+                else
+                    deleteTag.style.transition = "color 1s";
                 setTimeout(function () {
                     deleteTag.style.fontSize = "1em";
                     deleteTag.style.color = "white";
                 }, 1000);
             }
-            })
+        })
 
-}}
+    }
+}
 
 function httpDeleteAll(url) {
     var deleteAllTag = document.getElementById("deleteAllTag");
     deleteAllTag.style.color = "rgb(196, 255, 240)";
-    deleteAllTag.style.fontSize = "22px";
-    deleteAllTag.style.transition = "all 1s"
+    if (mediaScreen.matches != true) {
+        deleteAllTag.style.transition = "all 1s";
+        deleteAllTag.style.fontSize = "20px";
+    }
+    else
+        deleteAllTag.style.transition = "color 1s";
     setTimeout(function () {
         deleteAllTag.style.fontSize = "1em";
         deleteAllTag.style.color = "white";
@@ -179,8 +253,12 @@ function httpPutThisPassword(url) {
     var passwordToAdd = document.getElementById("passwordToAdd").value;
     if ((passwordToAdd != "" || /[a-zA-Z]/.test(passwordToAdd)) && passwordToAdd.length > 8) {
         pwToAdd.style.color = "rgb(196, 255, 240)";
-        pwToAdd.style.fontSize = "22px";
-        pwToAdd.style.transition = "all 1s"
+        if (mediaScreen.matches != true) {
+            pwToAdd.style.transition = "all 1s";
+            pwToAdd.style.fontSize = "20px";
+        }
+        else
+            pwToAdd.style.transition = "color 1s";
         setTimeout(function () {
             pwToAdd.style.fontSize = "1em";
             pwToAdd.style.color = "white";
@@ -191,10 +269,14 @@ function httpPutThisPassword(url) {
         httpGet('http://localhost:8080/api/v1/passwords', 0);
         return xml.responseText;
     }
-    else{
+    else {
         pwToAdd.style.color = "red";
-        pwToAdd.style.fontSize = "22px";
-        pwToAdd.style.transition = "all 1s"
+        if (mediaScreen.matches != true) {
+            pwToAdd.style.transition = "all 1s";
+            pwToAdd.style.fontSize = "20px";
+        }
+        else
+            pwToAdd.style.transition = "color 1s";
         setTimeout(function () {
             pwToAdd.style.fontSize = "1em";
             pwToAdd.style.color = "white";
